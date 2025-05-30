c2171d4978c9f5167e168b5e56d4b5b3a2beb7e2
rangeset <2432647223@qq.com>
2025-05-30 18:05:01 +0800

    Create some empty module file

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use log::{info, warn};
+
+fn main() {
+    info!("RaMail is STARTING");
+}
---
+++ src/tool_stack/encode_stack.rs
@@ -0,0 +1,9 @@
+const BASE64:[Char;64] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                          'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                          'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                          'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/',];
+fn encode_base64(str : String) -> String {
+    for byte in str.as_bytes() {
+        ;
+    }
+}
